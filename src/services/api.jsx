45b2297c8d8b6479b077ea7fc70801034140441f
kkrajid <kkrajid@gmail.com>
2024-12-02 15:53:29 +0530

    based url updated

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/';
+// const API_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://13.51.169.229/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
